5874e0c1551531ef0a9b33bce79f1d1a6d281c61
Ayush <ayush4002gupta@gmail.com>
2023-04-19 12:35:33 +0530

    first commit with simple contract and tested

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.4.1;
+
+contract SocialNetwork {
+    string public name;
+
+    constructor() public{
+        name="Dapp University is so cool";
+    }
+     
+    
+}
+
